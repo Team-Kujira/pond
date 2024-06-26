50368f58444c913b7a6c6a2fe21401a13a600e68
Starsquid <108214377+starsquidnodes@users.noreply.github.com>
2024-06-27 00:38:38 +0200

    Update cosmoshub version

--- pond/globals/versions.go
+++ pond/globals/versions.go
@@ -2,7 +2,7 @@ package globals
 
 var Versions = map[string]string{
 	"kujira":    "99f7924-2",
-	"cosmoshub": "v16.0.0-2",
+	"cosmoshub": "v17.2.0-1",
 	"terra2":    "v2.11.1-4",
 	"feeder":    "api-only-2",
 	"relayer":   "v2.5.2-2",
